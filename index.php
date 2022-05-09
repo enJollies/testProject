0be8291a8e934501a8eed40404917c173ea87e92
Eugene Kushch <jesthete78@gmail.com>
2022-05-09 22:24:50 +0300

    Add 2 new files

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+	require_once __DIR__.'/vendor/autoload.php';
+	
+	$faker = Faker\Factory::create('ru_RU');
+
+	for($i = 0; $i < 10; $i++){
+		
+		echo "<p>", $faker->text(100),"</p>";
+	}
+
+	echo $faker->inn10();
